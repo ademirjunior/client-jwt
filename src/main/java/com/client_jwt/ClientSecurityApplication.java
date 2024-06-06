06a60e3a3bb4d40d990647f8c9d6c6afcf2d47d2
Ademir Júnior <ademiralcojr@gmail.com>
2024-06-05 21:17:08 -0300

    first commit

---
+++ src/main/java/com/client_jwt/ClientSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.client_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientSecurityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/client_jwt/ClientSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
